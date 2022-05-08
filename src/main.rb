74e2ed62ace494ac2874abc5216e85746bb4a994
Creation6969 <89533900+Creation6969@users.noreply.github.com>
2022-05-08 10:54:01 -0300

    Update main.rb

--- src/main.rb
+++ src/main.rb
@@ -4,9 +4,9 @@ File.open(ARGV[0], "r") do |file|
   
   y.times do |i|
     if x[i]=="log>"
-      puts x[i+1]
+      puts x[i+1].gsub("::s::", " ")
     elsif x[i]=="log<"
-      puts x[i-1]
+      puts x[i-1].gsub("::s::", " ")
     elsif x[i]=="rep"
       x[i]=x[i-1].gsub(x[i+1], x[i+2])
       x[i-1]=x[i]
@@ -14,8 +14,13 @@ File.open(ARGV[0], "r") do |file|
       x[i+2]=x[i]
     elsif x[i]=="newline"
       x[i]="\n"
+    elsif x[i]=="exit"
+      exit()
+    elsif x[i]=="forceExit"
+      exit!()
+    elsif x[i]=="cfile"
+      x[i]=ARGV[0]
     end
   end
 end
-
 
