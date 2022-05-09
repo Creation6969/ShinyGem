7041f8f53009c3e46cab0847d6a5098cb9d43754
Creation6969 <89533900+Creation6969@users.noreply.github.com>
2022-05-09 12:26:41 -0300

    Update main.rb

--- src/main.rb
+++ src/main.rb
@@ -1,6 +1,7 @@
 File.open(ARGV[0], "r") do |file|
   x = file.read().split
   y = x.length()
+  vars = []
   
   y.times do |i|
     if x[i]=="log>"
@@ -20,6 +21,25 @@ File.open(ARGV[0], "r") do |file|
       exit!()
     elsif x[i]=="cfile"
       x[i]=ARGV[0]
+    elsif x[i]=="dup"
+      x[i+2]=x[i+1]
+    elsif x[i]=="empty"
+      # nothing!
+    elsif x[i]=="shift"
+      x[i]=x[i+1]
+    elsif x[i]=="log<<"
+      puts x[i-3].gsub("::s::", " ")
+    elsif x[i]=="valueof"
+      vars.push("#{x[i-1]} #{x[i+1]}")
+    elsif x[i]=="var"
+      v = vars.length()
+      v.times do |ind|
+        cv = vars[ind].split
+        if cv[0]==x[i+1]
+          x[i]=cv[1]
+          x[i+1]=x[i]
+        end
+      end
     end
   end
 end
